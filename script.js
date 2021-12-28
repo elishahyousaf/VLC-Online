109f978abeaf6843d049ed085f3e1bc4971e6e3b
ahmedraja1 <ahmedrajay@gmail.com>
2021-12-29 02:56:09 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & Pause video
+function toggleVideoStatus(){
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update Play / Pause Icon
+function updatePlayIcon(){
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+// Update Progress & Time Stamp
+function UpdateProgress(){
+  progress.value = (video.currentTime / video.duration) * 100;
+  
+// Get minutes
+let mins = Math.floor(video.currentTime / 60);
+if (mins < 10) {
+  mins = '0' + String(mins);
+}
+
+// Get seconds
+let secs = Math.floor(video.currentTime % 60);
+if (secs < 10) {
+  secs = '0' + String(secs);
+}
+
+timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+
+
+// Set video time to progress
+function setVideoProgress(){
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop Video
+function stopVideo(){
+  video.currentTime = 0;
+  video.pause();
+}
+
+// Left Right Arrows +5/-5
+forward=()=>{
+  skip(5);
+}
+
+backward=()=>{
+   skip(-5);
+}
+
+skip=(time)=>{
+  video.currentTime=video.currentTime+time;
+}
+
+
+
+
+// Event Listeners on Video
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('timeupdate', UpdateProgress);
+
+// Event Listener on Play
+play.addEventListener('click', toggleVideoStatus);
+
+// Event Listener on Stop
+stop.addEventListener('click', stopVideo);
+
+// Event Listener on Progress
+progress.addEventListener('change', setVideoProgress);
+
+
+
+
+// Event Listener for left & right keys
+document.addEventListener("keydown",(e)=>{
+  if(e.keyCode==37){       //left arrow
+      backward()
+  }else if(e.keyCode==39){ //right arrow
+      forward()
+  }
+}
+)
+
+
+// Event Listener for  Space
+document.addEventListener("keydown",(e)=>{
+  if (e.keyCode==32)
+  {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+}
+)
+
+
+
+  
+  
+ 
